1f877e6e43f2d1c7e86210c17426c5ddcd9b8b28
Jason Viger <jason.viger@gmail.com>
2019-04-10 03:59:38 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :summary, length: { maximum: 250 }
+  validates :content, length: { minimum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbaity?
+  
+  clickbait = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+  
+  def clickbaity?
+    if clickbait.none? { |word| title.include?(word) }
+      errors.add(:title, "needs to be clickbaity")
+    end
+   
+  end
+  
+  
 end
